7516673c6440332b8c93f2381b686eb5780f0e37
joelmcf <joel.mcfarland@gmail.com>
2020-12-12 06:12:33 -0600

    Cleaned up code and attempetd to synch frames to days.

--- trillium_recurvatum_map.R
+++ trillium_recurvatum_map.R
@@ -1,12 +1,24 @@
 ################################################################################
+# Purpose: Observation coordinates gif
+# Input data: December 10, 2020 downloads from iNat
+# Output: 
+#   - gif showing observations plotted over blank county-level map
+# Date: December 10, 2020
+################################################################################################################################################################
 # Trillium recurvatum 
 
 library(tidyverse)
+library(lubridate)
 library(scales)
 library(urbnmapr)
 library(gganimate)
+library(iNatTools)
 
-trillium <- read.csv("observations-123153.csv")
+trillium <- read.csv("observations-123153.csv")  %>%
+    mutate(observed_on_date = ymd(observed_on))
+
+typeof(trillium$observed_on) #integer
+typeof(trillium$observed_on_date) # double
 
 numdates <- trillium %>%
     distinct(observed_on)
@@ -16,6 +28,9 @@ numdates <- trillium %>%
 # latitude
 # longitude
 
+# Resources
+# -https://cran.r-project.org/web/packages/gganimate/gganimate.pdf
+
 ################################################################################
 # Create map
 
@@ -23,7 +38,7 @@ numdates <- trillium %>%
 states.il <- filter(states, state_name == 'Illinois')
 counties.il <- filter(counties, state_name == 'Illinois')
 
-trillium.map <- ggplot(counties.il, mapping = aes(long, lat, group = group)) + 
+trillium.map2 <- ggplot(counties.il, mapping = aes(long, lat, group = group)) + 
     # Add state border
     geom_polygon(data = states.il, mapping = aes(long, lat, group = group),
                  fill = NA, color = "#000000") +
@@ -43,32 +58,36 @@ trillium.map <- ggplot(counties.il, mapping = aes(long, lat, group = group)) +
           axis.title = element_blank(), 
           axis.text = element_blank(), 
           axis.ticks = element_blank(), 
-          legend.background = element_rect(fill="gray90")) + 
+          legend.background = element_rect(fill="gray90"))
+
+trillium.map1 <- trillium.map2 + 
     labs(subtitle = '{closest_state}')
 
-trillium.map + 
+trillium.map1 + 
     geom_point(data=trillium, aes(longitude, latitude), inherit.aes = FALSE, size=.5)
 
 ################################################################################
 # Create animation
 # Source example: https://medium.com/business-as-usual-at-solar-analytics/animating-time-series-on-a-map-using-solar-analytic-data-and-r-s-gganimate-package-7831dc3da9d2
-anim <- trillium.map +
-    geom_point(data=trillium, aes(longitude, latitude), color="#56d800",
+
+anim <- trillium.map1 +
+    geom_point(data=trillium, aes(longitude, latitude), 
+               pch=21, fill="#56d800",
+               color="black",
                inherit.aes = FALSE, size=3) + 
-    transition_states(observed_on, transition_length=0) + 
-    shadow_mark(color="#808080", size=1)
+    transition_states(observed_on_date, transition_length=0) + 
+    shadow_mark(fill="#808080", color="#808080", size=1)
 
 anim <- animate(anim, nframes=87, dur=50)
-
-anim
 
 # Export gif
 anim_save("trillium.gif", anim)
 
 
 ################################################################################
-# Formatting cleanup
+# Formatting cleanup notes
 # Put border around initial geom_point: https://stackoverflow.com/questions/10437442/place-a-border-around-points
+
 # Show static frames for days without data
 # Base initial color on phenology annotations
 # Put data in Month XX, XXXX format
@@ -82,10 +101,26 @@ anim_save("trillium.gif", anim)
 ## Sanguinaria canadensis
 ## Erythronium albidum
 
+################################################################################
+# Attempt to synch frames to days
+
+trillium.filtered <- trillium %>%
+    filter(observed_on_date <= "2020-06-15")
+
+anim.days <- trillium.map2 +
+    geom_point(data=trillium.filtered, aes(longitude, latitude), 
+               pch=21, fill="#56d800",
+               color="black",
+               inherit.aes = FALSE, size=3) + 
+    transition_time(observed_on_date) + 
+    shadow_mark(fill="#808080", color="#808080", size=1)
 
 
+anim.days <- animate(anim.days, nframes=87, dur=60)
 
+anim.days
 
+# Export gif
+anim_save("trillium_days.gif", anim.days)
 
-
-
+################################################################################
