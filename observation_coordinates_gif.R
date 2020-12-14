c5a2cbb10117cfd635ee526cd175e22a33a1b036
joelmcf <joel.mcfarland@gmail.com>
2020-12-13 23:14:03 -0600

    Revised title, subtitle, and caption layout; created flowering-only version of Sanguinaria canadensis gif.

--- observation_coordinates_gif.R
+++ observation_coordinates_gif.R
@@ -37,15 +37,18 @@ map <- ggplot(counties.il, mapping = aes(long, lat, group = group)) +
     # Set map projection
     coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
     # Adjust theme elements
-    theme(plot.title = element_text(hjust = 0.5, face="bold"),
-          plot.subtitle = element_text(hjust = 0.5, ),
+    theme(#plot.title = element_text(hjust = 0.5, face="bold"),
+          #plot.subtitle = element_text(hjust = 0.5, ),
           panel.background = element_blank(),
           panel.border = element_blank(), 
           panel.grid.major = element_blank(), 
           axis.title = element_blank(), 
           axis.text = element_blank(), 
           axis.ticks = element_blank(), 
-          legend.background = element_rect(fill="gray90"))
+          legend.background = element_rect(fill="gray90"), 
+          title = element_text(size=12), 
+          plot.subtitle = element_text(size=11),
+          plot.caption = element_text(size=12, hjust = 0.60))
 
 ################################################################################
 # Create animation
@@ -67,8 +70,9 @@ map.trillium <- map +
                inherit.aes = FALSE, size=3) + 
     transition_states(observed_on_date, transition_length=0) + 
     shadow_mark(fill="#808080", color="#808080", size=1) +
-    ggtitle("Trillium recurvatum observations") + 
-    labs(subtitle = '{closest_state}')
+    labs(title = "Prairie Trillium observations", 
+         subtitle = expression(italic("(Trillium recurvatum)")),
+         caption = "{closest_state}")
 
 map.trillium <- animate(map.trillium, nframes=87, dur=50)
 
@@ -95,26 +99,23 @@ anim_save("trillium.gif", map.trillium)
 
 ################################################################################
 # Attempt to synch frames to days
-
-trillium.filtered <- trillium %>%
-    filter(observed_on_date <= "2020-06-15")
-
-anim.days <- map +
-    geom_point(data=trillium.filtered, aes(longitude, latitude), 
-               pch=21, fill="#56d800",
-               color="black",
-               inherit.aes = FALSE, size=3) + 
-    transition_time(observed_on_date) + 
-    shadow_mark(fill="#808080", color="#808080", size=1)
-
-
-anim.days <- animate(anim.days, nframes=87, dur=60)
-
-anim.days
-
+#
+#trillium.filtered <- trillium %>%
+#    filter(observed_on_date <= "2020-06-15")
+#
+#anim.days <- map +
+#    geom_point(data=trillium.filtered, aes(longitude, latitude), 
+#               pch=21, fill="#56d800",
+#               color="black",
+#               inherit.aes = FALSE, size=3) + 
+#    transition_time(observed_on_date) + 
+#    shadow_mark(fill="#808080", color="#808080", size=1)
+#
+#anim.days <- animate(anim.days, nframes=87, dur=60)
+#
 # Export gif
-anim_save("trillium_days.gif", anim.days)
-
+# anim_save("trillium_days.gif", anim.days)
+#
 ################################################################################
 claytonia <- read.csv("observations-123415.csv")  %>%
     mutate(observed_on_date = ymd(observed_on))
@@ -130,8 +131,9 @@ map.claytonia <- map +
                inherit.aes = FALSE, size=3) + 
     transition_states(observed_on_date, transition_length=0) + 
     shadow_mark(fill="#808080", color="#808080", size=1) +
-    ggtitle("Claytonia virginica observations") + 
-    labs(subtitle = '{closest_state}')
+    labs(title = "Virginia Spring Beauty observations", 
+         subtitle = expression(italic("(Claytonia virginica)")),
+         caption = "{closest_state}")
 
 map.claytonia <- animate(map.claytonia, nframes=78, dur=50)
 
@@ -153,10 +155,42 @@ map.sanguinaria <- map +
                inherit.aes = FALSE, size=3) + 
     transition_states(observed_on_date, transition_length=0) + 
     shadow_mark(fill="#808080", color="#808080", size=1) +
-    ggtitle("Sanguinaria canadensis observations") + 
-    labs(subtitle = '{closest_state}')
+    labs(title = "Bloodroot observations", 
+         subtitle = expression(italic("(Sanguinaria canadensis)")),
+         caption = "{closest_state}")
 
 map.sanguinaria <- animate(map.sanguinaria, nframes=89, dur=50)
 
 # Export gif
 anim_save("sanguinaria.gif", map.sanguinaria)
+
+################################################################################
+# Sanguinaria canadensis data with phenology annotations
+sanguinaria.phen <- iNat(project = "illinois-botanists-big-year-2020", 
+                              taxon_id=51044, 
+                              term_id = 12, 
+                              term_value_id = 13) %>%
+    mutate(latitude = gsub(",.*$", "", location),
+           longitude = gsub("*$.,", "", location)) %>% 
+    select(location, latitude, longitude)
+
+numdates <- sanguinaria.phen %>%
+    distinct(observed_on)
+# 38
+
+
+map.sanguinaria.phen <- map +
+    geom_point(data=sanguinaria.phen, aes(longitude, latitude), 
+               pch=21, fill="#56d800",
+               color="black",
+               inherit.aes = FALSE, size=3) + 
+    transition_states(observed_on_date, transition_length=0) + 
+    shadow_mark(fill="#808080", color="#808080", size=1) +
+    labs(title = "Bloodroot observations", 
+         subtitle = expression(italic("(Sanguinaria canadensis)")),
+         caption = "{closest_state}")
+
+map.sanguinaria.phen <- animate(map.sanguinaria.phen, nframes=38, dur=50)
+
+# Export gif
+anim_save("sanguinaria.gif", map.sanguinaria.phen)
